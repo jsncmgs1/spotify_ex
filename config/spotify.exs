eafd19bab9d3baba99e2ff49bc9137f361334b6b
Jason Cummings <jasoncummings86@gmail.com>
2015-12-02 16:47:58 -0500

    Use correct URI encoding

--- config/spotify.exs
+++ config/spotify.exs
@@ -1,4 +1,4 @@
 use Mix.Config
 
 config :spotify_ex, callback_url: "http://localhost:4200/authenticate"
-config :spotify_ex, scopes: []
+config :spotify_ex, scopes: ["playlist-modify-public", "playlist-modify-private"]
--- lib/spotify/authorization.ex
+++ lib/spotify/authorization.ex
@@ -15,9 +15,9 @@ defmodule Spotify.Authorization do
     "https://accounts.spotify.com/authorize?client_id=#{Spotify.client_id}&response_type=code&redirect_uri=#{Spotify.callback_url}"
   end
 
-  defp scopes do
+  def scopes do
     Application.get_env(:spotify_ex, :scopes)
      |> Enum.join(" ")
-     |> URI.encode_www_form
+     |> URI.encode
   end
 end
