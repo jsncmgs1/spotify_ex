d4099414e356680ece8272d776d466bab7aed7d6
Jason Cummings <jasoncummings86@gmail.com>
2016-07-21 16:42:11 -0400

    Load Conn and PoisonResponse during tests

--- config/test.exs
+++ config/test.exs
@@ -4,5 +4,8 @@ Path.wildcard("test/mocks/**")
 |> Enum.filter(&String.match?(&1, ~r/mock/))
 |> Enum.each(&Code.require_file("../#{&1}", __DIR__))
 
+Code.ensure_loaded(Plug.Conn)
+Code.ensure_loaded(HTTPoison.Response)
+
 config :spotify_ex,
   auth_client: Spotify.AuthenticationClientMock
