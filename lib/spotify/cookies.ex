d9b92fd2ad04e67ebfb5e22b0dbc3b39fb1714da
Jason Cummings <jasoncummings86@gmail.com>
2016-07-20 23:07:03 -0400

    Cookies documentation

--- lib/spotify/cookies.ex
+++ lib/spotify/cookies.ex
@@ -1,20 +1,45 @@
 defmodule Spotify.Cookies do
+  @moduledoc """
+    Convinience setters and getters for auth cookies
+  """
+
+  @doc """
+  Sets the refresh token and access token
+
+    ##Example:
+      iex> Spotify.Cookies.set_cookies(conn, %{refresh_token: rt, access_token: at})
+  """
+  def set_cookies(conn, map)
   def set_cookies(conn, %{refresh_token: refresh_token, access_token: access_token}) do
     conn |> set_refresh_cookie(refresh_token) |> set_access_cookie(access_token)
   end
 
+  @doc """
+  Sets the refresh token
+  """
+  def set_refresh_cookie(conn, string)
   def set_refresh_cookie(conn, refresh_token) do
     Plug.Conn.put_resp_cookie(conn, "spotify_refresh_token", refresh_token)
   end
 
+  @doc """
+  Sets the access token
+  """
+  def set_access_cookie(conn, string)
   def set_access_cookie(conn, access_token) do
     Plug.Conn.put_resp_cookie(conn, "spotify_access_token", access_token)
   end
 
+  @doc """
+  Gets the access token
+  """
   def get_access_cookie(conn) do
     conn.cookies["spotify_access_token"]
   end
 
+  @doc """
+  Gets the access token
+  """
   def get_refresh_cookie(conn) do
     conn.cookies["spotify_refresh_token"]
   end
