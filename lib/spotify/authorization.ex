0f2530f60849a8c0a5f7c6199468234e585986df
Jason Cummings <jasoncummings86@gmail.com>
2016-07-20 22:53:13 -0400

    Add docs to Authorization

--- lib/spotify/authorization.ex
+++ lib/spotify/authorization.ex
@@ -1,13 +1,42 @@
 defmodule Spotify.Authorization do
+  @moduledoc """
+  Authorizes your app with Spotify
+
+  Spotify needs to verify your client id, and that your redirect uri
+  matches what you set in your app settings (in the Spotify App dashboard).
+  This is an external call, url provided by the `url` function.
+  """
+
+  @doc """
+   If you specified scopes in your config, uses scoped auth.
+   Otherwise, unscoped. Use this function to make the redirect to
+   Spotify for authorization.
+
+   ## Example:
+
+       defmodule OAuthController do
+         # ...
+
+         def authorize do
+           redirect conn, external: Spotify.Authorization.url
+         end
+       end
+  """
   def url do
     if scopes != "" do
-      auth_with_scopes
+      scoped_auth
     else
       scopeless_auth
     end
   end
 
-  defp auth_with_scopes do
+  defp scopes do
+    Application.get_env(:spotify_ex, :scopes)
+     |> Enum.join(" ")
+     |> URI.encode
+  end
+
+  defp scoped_auth do
     "https://accounts.spotify.com/authorize?client_id=#{Spotify.client_id}&response_type=code&redirect_uri=#{Spotify.callback_url}&scope=#{scopes}"
   end
 
@@ -15,10 +44,4 @@ defmodule Spotify.Authorization do
     "https://accounts.spotify.com/authorize?client_id=#{Spotify.client_id}&response_type=code&redirect_uri=#{Spotify.callback_url}"
   end
 
-  def scopes do
-    Application.get_env(:spotify_ex, :scopes)
-     |> Enum.join(" ")
-     |> URI.encode
-  end
 end
-
