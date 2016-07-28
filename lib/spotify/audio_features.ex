3d21da20147fa49f16c8442d91f98df259356744
Jason Cummings <jasoncummings86@gmail.com>
2016-07-28 16:29:11 -0400

    Add AudioFeatures

---
+++ lib/spotify/audio_features.ex
@@ -0,0 +1,22 @@
+defmodule AudioFeatures do
+  defstruct ~w[
+    acousticness
+    analysis_url
+    danceability
+    duration_ms
+    energy
+    id
+    instrumentalness
+    key
+    liveness
+    loudness
+    mode
+    speechiness
+    tempo
+    time_signature
+    track_href
+    type
+    uri
+    valence
+  ]a
+end
