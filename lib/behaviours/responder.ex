54b0620017ae06439851574a5de004c017527ad4
Jason Cummings <jasoncummings86@gmail.com>
2016-07-29 13:29:19 -0400

    Add Responder

---
+++ lib/behaviours/responder.ex
@@ -0,0 +1,19 @@
+defmodule Responder do
+  @callback build_response(map) :: any
+
+  defmacro __using__(_) do
+    quote do
+      def handle_response({ message, %HTTPoison.Response{ status_code: code, body: body }})
+      when code in 400..499 do
+        { message, Poison.decode!(body)}
+      end
+
+      @doc false
+      def handle_response({ :ok, %HTTPoison.Response{ status_code: _code, body: body }}) do
+        response = body |> Poison.decode! |> build_response
+
+        { :ok, response }
+      end
+    end
+  end
+end
