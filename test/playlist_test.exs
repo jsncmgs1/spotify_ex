9c07ac7960cbcfea485f68e3141eb020f5df1193
Jason Cummings <jasoncummings86@gmail.com>
2016-07-30 13:40:38 -0400

    Playlist non doc tests

---
+++ test/playlist_test.exs
@@ -0,0 +1,45 @@
+defmodule PlaylistTest do
+  use ExUnit.Case
+  alias Spotify.Playlist
+
+  describe "build_response/1" do
+    test "the API returns a collection" do
+      actual = Playlist.build_response(response_body_with_collection)
+
+      collection = [%Playlist{name: "foo"}, %Playlist{name: "bar"}]
+      playlists = Enum.map(collection, &playlist/1)
+
+      expected = Map.merge(%Paging{}, %Paging{items: playlists})
+
+      assert actual == expected
+    end
+
+    test "the API returns a single playlist" do
+      actual = Playlist.build_response(response_body_with_playlist)
+      expected = playlist(%Playlist{name: "foo"})
+
+      assert actual == expected
+    end
+  end
+
+  test "%Spotify.Playlist{}" do
+    attrs = ~w[collaborative description external_urls followers
+    href id images name owner public snapshot_id tracks type uri]a
+
+    expected = Map.from_struct(%Playlist{}) |> Map.keys
+
+    assert expected == attrs
+  end
+
+  def playlist(playlist), do: Map.merge(%Playlist{}, playlist)
+
+  def response_body_with_collection do
+    %{
+      "playlists" => %{
+        "items" => [%{ "name" => "foo" }, %{ "name" => "bar" } ]
+      }
+    }
+  end
+
+  def response_body_with_playlist, do: %{ "name" => "foo" }
+end
