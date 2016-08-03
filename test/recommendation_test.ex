fd95aadd3da88fc16d41567b2234b3943f03eb05
Jason Cummings <jasoncummings86@gmail.com>
2016-08-02 20:58:07 -0400

    Add Recommendation

---
+++ test/recommendation_test.ex
@@ -0,0 +1,17 @@
+defmodule RecommendationTest do
+  use ExUnit.Case
+  alias Spotify.{Recommendation, Track, Seed}
+
+  test "%Recommendation{}" do
+    actual = %Recommendation{} |> Map.from_struct |> Map.keys
+    expected = ~w[seeds tracks]a
+
+    assert actual == expected
+  end
+
+  test "build_response/1" do
+    response = %{"seeds" => [%{"id" => "foo"}], "tracks" => [%{"id" => "bar"}]}
+    actual = Recommendation.build_response(response)
+    expected = %Recommendation{tracks: [%Track{id: "bar"}], seeds: [%Seed{id: "foo"}]}
+  end
+end
